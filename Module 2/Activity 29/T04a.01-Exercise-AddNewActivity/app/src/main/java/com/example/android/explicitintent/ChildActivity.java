be672baec1db0742c2fa267144b02bc873f42fc2
prathyushab14 <prathyushabathula14@msitprogram.net>
2019-03-16 10:01:22 +0530

    module2

--- Module 2/Activity 29/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Module 2/Activity 29/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -2,12 +2,18 @@ package com.example.android.explicitintent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
 
 public class ChildActivity extends AppCompatActivity {
+    private TextView mDisplayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        mDisplayText = (TextView) findViewById(R.id.tv_display);
     }
 }
